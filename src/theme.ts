04ccee959b2514edcf515e8eb4a8ed264b39a5ab
Lucas Adams <lucas.adams@agrotis.com>
2017-01-25 10:42:35 -0200

    Ajustes deploy: - Dependencia do @type do material design ajustada apenas para dev;
    Ajustes de Warnign
    - Espaço final de arquivo;
    - Comentarios com letras maiusculas;

--- src/theme.ts
+++ src/theme.ts
@@ -4,14 +4,12 @@ function themeConfig($mdThemingProvider: angular.material.IThemingProvider) {
 
     $mdThemingProvider.theme('default')
         .primaryPalette('teal', {
-            'default': '400', // by default use shade 400 from the pink palette for primary intentions
-            'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
-            'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
-            'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+            'default': '400',
+            'hue-1': '100',
+            'hue-2': '600',
+            'hue-3': 'A100'
         })
-        // If you specify less than all of the keys, it will inherit from the
-        // default shades
         .accentPalette('amber', {
-            'default': '200' // use shade 200 for default, and keep all other shades the same
+            'default': '200'
         });
 }
